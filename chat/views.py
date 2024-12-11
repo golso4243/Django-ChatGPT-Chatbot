c8aff18ee0d3153a80881d6a92314408e8345d75
Gage Olson <gage.a.olson@gmail.com>
2024-12-11 11:02:59 -0600

    Form Processing

--- chat/views.py
+++ chat/views.py
@@ -4,4 +4,7 @@ from django.shortcuts import render
 
 
 def home(request):
+    if request.method == 'POST':
+        question = request.POST.get('question')
+        return render(request, 'home.html', {"question": question})
     return render(request, 'home.html', {})
